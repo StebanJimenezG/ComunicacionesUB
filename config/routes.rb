cf510e0492b59059e74f80ce8c17a13513d8af9a
StebanJimenezG <teban.jg@gmail.com>
2017-05-09 21:55:57 -0500

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :members
   resources :committees
   resources :activities
   resources :type_fs
   resources :type_files
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
